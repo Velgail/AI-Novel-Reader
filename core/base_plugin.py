3e76e83db311afb94beb8828624fa873e32eeb40
Velgail <Velgail@users.noreply.github.com>
2025-05-08 00:04:13 +0900

    Revert "Initial project setup and core system foundation (#1)"
    This reverts commit 212a1e09781e9830522044d1212577c71f8d946d.

--- core/base_plugin.py
+++
@@ -1,6 +0,0 @@
-class BasePlugin:
-    def __init__(self, name):
-        self.name = name
-
-    def execute(self, *args, **kwargs):
-        raise NotImplementedError("Execute method must be implemented by the plugin.")
